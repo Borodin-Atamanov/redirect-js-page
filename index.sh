007ad1508ccaf58fc5ab7891031b3c8dc1ce46c6
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2025-02-13 17:09:10 -0300

    negime-28-2502131709

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='dorasc-27-2502131707'; 
+declare -g -x script_version='negime-28-2502131709'; 
 
 function run_task ()
 {
