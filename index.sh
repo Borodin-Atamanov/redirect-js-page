d7007d66b028026f324ba7ee32f06ba009eff607
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2025-02-17 23:04:43 -0300

    vutato-29-2502172304

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='negime-28-2502131709'; 
+declare -g -x script_version='vutato-29-2502172304'; 
 
 function run_task ()
 {
